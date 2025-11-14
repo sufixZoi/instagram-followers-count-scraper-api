341b36bb42aee6ee80068cc38d30e88813d353f7
sufixZoi <sxujixl@feiti.online>
2025-11-14 09:55:41 -0500

    feat: add instagram-followers-count-scraper/src/extractors/utils.py

---
+++ instagram-followers-count-scraper/src/extractors/utils.py
@@ -0,0 +1,9 @@
+thonimport json
+
+def load_json(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+def save_json(data, file_path):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
